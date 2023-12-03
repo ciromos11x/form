8d525fe4b247b68589e98cd3a73d664e63a9f400
Ciro Moscarella <ciro.moscarella@icloud.com>
2023-12-03 12:12:12 +0100

    server

--- src/App.js
+++ src/App.js
@@ -1,16 +1,16 @@
-// client/src/App.js
-
 import React from 'react';
-import './App.css';
-import Form from './Form';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Form from './Form'; // Importa il componente Form
+import Home from './Home';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <Form />
-      </header>
-    </div>
+    <Router>
+      <Switch>
+        <Route path='/' exact component= {Home} />
+        <Route path='/form' component = {Form} />
+      </Switch>
+    </Router>
   );
 }
 
